99bb0346991d56116b10777c3f82da0488452248
Roman Fedorov <roma.volt1125@gmail.com>
2026-02-24 09:58:59 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+/*import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
+import heroImg from './assets/hero.png';*/
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <header className="flex-container">
+        <h1>Here should be a name</h1>
+      </header>
+      <main>
+        <h2>Here are my infos:</h2>
+        <p>Random text obviously not about me</p>
+      </main>
+      <footer>No rights</footer>
+    </body>
+  );
+}
+
+export default App;
